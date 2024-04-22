2314a55b089aeca9cdf01a68dd7069437b6efe9c
LackHaus <thomas.lacasse08@gmail.com>
2024-04-22 16:56:51 -0400

    Working on issue saving xosawdm file on FTB-2 Pro

--- FTB-2_Pro_test.py
+++ FTB-2_Pro_test.py
@@ -3,4 +3,5 @@ import InstrumentControl
 ltb2 = InstrumentControl.LTB('169.254.54.44', 5025).platform
 osa = InstrumentControl.OSA("LTB8", ltb2, 0)
 
-osa.inband_Analysis([1550, 1560], 100, "C:\OSA")
+for i in range(10):
+    osa.inband_Analysis([1550, 1560], 10, "C:Users/Public/test"+str(i)+".xosawdm")
